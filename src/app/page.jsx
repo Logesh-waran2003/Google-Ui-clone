6c8ffefd67f9f085f23b8f40ff2150fc656590cb
Logesh <logeshwaransrini@gmail.com>
2024-02-15 01:44:36 +0530

    added homesearch, feeling lucky func

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Footer from "@/components/Footer";
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
 
 export default function Home() {
   return (
     <div className="">
       <HomeHeader/>
+      <HomeSearch/>
       <Footer/>
     </div>
 );
