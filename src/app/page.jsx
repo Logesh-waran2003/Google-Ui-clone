4f38f36a00c6f019d95d0c8f11cfd097418f3672
Logesh <logeshwaransrini@gmail.com>
2024-02-15 01:08:42 +0530

    added footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/Footer";
 import HomeHeader from "@/components/HomeHeader";
 
 export default function Home() {
   return (
     <div className="">
-      <HomeHeader></HomeHeader>
+      <HomeHeader/>
+      <Footer/>
     </div>
 );
 }
